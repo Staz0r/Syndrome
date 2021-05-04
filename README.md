06029fec37051782a61a5bc78d1b591d81314752
Staz0r <51358327+Staz0r@users.noreply.github.com>
2021-05-04 21:37:46 +0700

    removed everything

--- README.md
+++
@@ -1,3 +0,0 @@
-# Syndrome
-
-A powerful bot used to increase your server members with a lot of bots
--- syndrome.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const Data = fetch('./package.json');
-
-const client = new Discord.Client();
-const Dev = process.env.OWNER;
-const Dev2 = process.env.OWNER2;
-const prefix = process.env.PREFIX;
-const version = Data.version;
